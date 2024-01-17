8075a90598aed5538d9af60e03e308a0cf27ad24
Pradeep5439 <spradeepkumar5439@gmail.com>
2024-01-17 13:46:28 +0530

    fourth commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "username" {
+    description = "admin username"
+    type = string
+    default = "adminuser"  
+}
+
+variable "password" {
+    description = "admin password"
+    type = string
+    default = "Pradeep5439"
+  
+}
